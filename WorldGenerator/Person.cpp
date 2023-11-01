ca80725704895498e9c4f5a771655b39b08517fd
Ferg <108194508+Fergclan@users.noreply.github.com>
2023-10-31 20:53:32 -0400

    Creates person class, does not work.Add project files.

---
+++ WorldGenerator/Person.cpp
@@ -0,0 +1,30 @@
+#include "Person.h"
+#include <iostream>
+#include <string>
+
+class Person::Person
+{
+	//Race race;
+	//Culture culture;
+	//Religion religion;
+	//State homeState;
+	//Town home;
+
+	// UNIQUE TO A PERSON // 
+	public:
+		string name;
+		string gender;
+		int age;
+
+		Person::Person()
+		{
+			name = "dave";
+			gender = "male";
+			age = 20;
+		}
+
+		void Person::printPerson()
+		{
+			std::cout << "my name is " << name << ", I am " << age << " and I am a " << gender << ".\n";
+		}
+}
---
+++ WorldGenerator/Person.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class Person
+{
+	public:
+		string name;
+		string gender;
+		int age;
+		Person();
+		void printPerson();
+
+};
+
